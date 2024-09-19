81b16664f20f2ff042626e5279febf5d069583fd
Jordan Patton <jordanpatton@gmail.com>
2024-09-19 17:27:31 -0400

    reorganize notebooks and scripts

--- lenovo_y27q20/scripts/report_osd_status.bat
+++ lenovo_y27q20/scripts/report_osd_status.bat
@@ -1,5 +1,5 @@
 @ECHO off
 TITLE Lenovo Y27q-20 On-Screen Display Status
-python ..\main.py lenovo_y27q20_report_osd_status
+python ..\..\main.py lenovo_y27q20_report_osd_status
 ECHO(
 PAUSE
--- scripts/lenovo_y27q20_set_day_mode.bat
+++
@@ -1,8 +0,0 @@
-@ECHO off
-TITLE Lenovo Y27q-20 Day Mode
-python ..\main.py lenovo_y27q20_set_day_mode
-ECHO Enabled day mode.
-ECHO(
-python ..\main.py lenovo_y27q20_report_osd_status
-ECHO(
-PAUSE
